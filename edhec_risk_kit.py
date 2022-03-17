ac539a9c44fbcfbe4fba1cbfff16856ca38025da
Chatchai Shaetan <chatchaishaetan@gmail.com>
2022-03-17 22:46:06 +0700

    Add week2 lab

--- edhec_risk_kit.py
+++ edhec_risk_kit.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def drawdown(return_series: pd.Series, initial_wealth: int = 1000) -> pd.Series:
@@ -25,3 +26,32 @@ def get_hfi_returns() -> pd.DataFrame:
     hfi = hfi / 100
     hfi.index = hfi.index.to_period('M')
     return hfi
+
+def get_ind_returns() -> pd.DataFrame:
+    ind_df = pd.read_csv('data/ind30_m_vw_rets.csv', header=0, index_col=0, parse_dates=True) / 100
+    ind_df.index = pd.to_datetime(ind_df.index, format='%Y%m').to_period('M')
+    ind_df.columns = ind_df.columns.str.strip()
+    return ind_df
+
+
+def annualize_return(return_series: pd.Series) -> float:
+    compounded_growth = (1 + return_series).prod()
+    n_period = len(return_series)
+    return compounded_growth**(12 / n_period) - 1
+
+def annualize_voltatility(return_series: pd.Series) -> float:
+    return return_series.std() * np.sqrt(12)
+
+def sharpe_ratio(return_series: pd.Series, risk_free_rate: float = 0.02) -> float:
+    annualize_risk_free = (1 + risk_free_rate) ** (1 / 12) - 1
+    excess_returns = return_series - annualize_risk_free
+    annualized_excess_return = annualize_return(excess_returns)
+    annualized_voltatility = annualize_voltatility(return_series)
+    return annualized_excess_return / annualized_voltatility
+
+
+def portfolio_return(weights, returns):
+    return weights.T @ returns
+
+def portfolio_volatility(weights, cov):
+    return (weights.T @ cov @ weights)**0.5
